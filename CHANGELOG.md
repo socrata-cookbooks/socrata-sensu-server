fd6c92c060b2f442c608d0f81bc9662d70d325e3
Jonathan Hartman <j@p4nt5.com>
2016-02-18 14:47:41 -0800

    Initial commit; a cookbook skeleton

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Socrata Sensu Server Cookbook CHANGELOG
+=======================================
+
+v0.0.1 (2016-02-18)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,18 @@
+# Encoding: UTF-8
+
+name             'socrata-sensu-server'
+maintainer       'Jonathan Hartman'
+maintainer_email 'jonathan.hartman@socrata.com'
+license          'apache2'
+description      'Installs/Configures socrata-sensu-server'
+long_description 'Installs/Configures socrata-sensu-server'
+version          '0.0.1'
+
+depends          'sensu', '~> 2.11'
+
+supports         'ubuntu'
+supports         'debian'
+supports         'centos'
+supports         'redhat'
+supports         'fedora'
+supports         'amazon'
